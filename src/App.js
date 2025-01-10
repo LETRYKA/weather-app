435537d3f84001d8752e08c7a58348423c88b560
LETRYKA <helloaryka@gmail.com>
2025-01-10 10:46:55 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="main-container">
+      <header className="header">
+        <h1 class="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
